62cab1afd481bbd8017f831c3eaa2d28e8d0a6a7
Marianom988 <marianom988@hotmail.com>
2023-06-30 11:29:09 +0100

    Added code to read user input

---
+++ basics/input/User_input.py
@@ -0,0 +1,11 @@
+#Ask user to enter their name
+print("What is your name human?")
+name=input()
+print("It's nice to see you human!"+name+".")
+#Read in user's name
+name=input("What is your name human?")
+print("It's nice to see you human!",name, ".")
+#Format function
+print("What is your name human?")
+name=input()
+print("It's nice to see you human {}!". format(name))
