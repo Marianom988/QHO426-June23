da82ba07bf756065fc30458acfbc9133480abc32
Marianom988 <marianom988@hotmail.com>
2023-06-30 12:29:19 +0100

    Added code to demonstrate the use of different data types

--- my_git_project/my file.py
+++ my_git_project/my file.py
@@ -1,3 +1,10 @@
-print("Please enter your name:")
-x = float
-print("Welcome {} to QHO426".format(x))
+print("what is your name?")
+name = str(input())
+print("How old are you?")
+age = int(input())
+print("How tall are you?")
+height = float(input())
+print("How much do you weight?")
+weight = float(input())
+bmi = weight/(height*height)
+print(f"Hi {name} you are {age} years old, your weight is {weight} kg , your height is {height} cm and your bmi is {bmi}! ")
