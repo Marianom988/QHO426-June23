5654e416d99eb095967119d49b6fb129db8b8808
Marianom988 <marianom988@hotmail.com>
2023-06-30 10:02:01 +0100

    Added code to display escape characters

--- basics/output/escape_characters.py
+++ basics/output/escape_characters.py
@@ -1,7 +1,9 @@
 #Displays escape characters
 print("\n displays a new line")
-print("\n displays a new line")
 print("\t displays a tab")
 print("\\ displays a backslash")
 print("\" displays a double quote")
 print("\' displays a single quote")
+#practice
+print("\n")
+print("\"I am Beep!\"")
