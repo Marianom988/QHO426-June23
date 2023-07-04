2de3dbe9f5921995767ede4abbf4ca23af7bcf1e
Marianom988 <marianom988@hotmail.com>
2023-07-04 11:20:26 +0100

    Week 2 seminar done

---
+++ basics/Decisions/and_operator.py
@@ -0,0 +1,6 @@
+noise = input("What did I hear? ")
+sight = input("What did I see? ")
+if noise == "grr" and sight == "two red eyes":
+    print("There is a scary creature I should get out of here!")
+else:
+    print("I am little scared, but I will continue.")
---
+++ my_git_project/my file2.py
@@ -0,0 +1,5 @@
+
+adventure = input("What kind of book is this?")
+if input("adventures"):
+    print("I like adventures book!")
+print("Finished reading book")
