eeade9021bfcc285f091840b3cac26e145839611
Marianom988 <marianom988@hotmail.com>
2023-07-05 11:21:20 +0100

    Practicing

---
+++ my_git_project/if_elif_else.py
@@ -0,0 +1,16 @@
+a = int(input("Write a number: "))
+b = int(input("Write another number: "))
+if a>b and a + b ==6:
+    print("WoW")
+
+elif a>b!=6:
+    print("Try again")
+
+elif a<b:
+    print("The second one is bigger")
+
+else:
+    print("The numbers are Equal")
+
+print("Done!")
+
--- my_git_project/my file2.py
+++ my_git_project/my file2.py
@@ -1,5 +1,13 @@
 
-adventure = input("What kind of book is this?")
-if input("adventures"):
-    print("I like adventures book!")
-print("Finished reading book")
+paint = input("Towards which direction should I paint ( up, down, left or right)? ")
+if paint == "up":
+    print("I'm painting up")
+
+elif paint == "down":
+    print("I'm painting down")
+
+elif paint == "left":
+    print("I'm painting left")
+
+elif paint == "right":
+    print("I'm painting right")
