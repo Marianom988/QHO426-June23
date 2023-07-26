b26591514b0633601f49abb3af073d82b990c3e5
Marianom988 <marianom988@hotmail.com>
2023-07-26 11:56:58 +0100

    Last exercise lecture w5

--- Demos/w5_tinder.py
+++ Demos/w5_tinder.py
@@ -1,4 +1,23 @@
 def interests():
-    input("tell me the interest")
+    print("Enter all interests, one after the other or \"stop\" to stop:  ")
     s1 = set()
-    
+    interest = ""
+    while True:
+        interest = input()
+        if interest.lower() == "stop":
+            break
+        s1.add(interest)
+    return s1
+
+def run():
+    print("Frist person: ")
+    p1 = interests()
+    print("Second person :")
+    p2 = interests()
+    common = p1.intersection(p2)
+    if len(common) >= 3:
+        print(f"You are a match made in heaven! you have {len(common)} hobbies in common")
+    else:
+        print("Oh no! it will probably not work. Find other human.")
+
+run()
