8b6ae942bd890412670dc69244ac7170a3ea6201
Marianom988 <marianom988@hotmail.com>
2023-08-21 11:19:06 +0100

    w9_animation

--- Demos/w8_lecture.py
+++ Demos/w8_lecture.py
@@ -1,3 +1,7 @@
 import matplotlib.pyplot as plt
 with open ("temps.txt", "w") as f:
-    f.write("Day Temperature\n")
+    g = {"Day ":"Temperature"}
+    for i in g.items(7) :
+     f.writelines(g.keys())
+     f.writelines(g.values())
+     print(g.keys(),g.values())
